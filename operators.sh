11a8dfe6316e5c5495a6b77081bc1daf3e6557c9
JRV-XVI <jrv.software@outlook.com>
2025-04-23 07:37:57 -0600

    Add literals & operators

--- operators.sh
+++ operators.sh
@@ -3,14 +3,19 @@
 input="input.py"
 output="output.html"
 
-echo "<html><body><pre>" > "$output"
+echo "<html><body><pre style=\"font-family: monospace\">" > "$output"
 
 while IFS= read -r line; do
-    # Operators
-    highlighted=$(echo "$line" | sed -E 's/\b(\+|\-|\*|\/|%|\*\*|\/\/|==|!=|<|>|\<=|\>=|and|or|not|=|\+=|\-=|\*=|\/=|&=|\|=|\^=|>>=|\*\*=|&|\||\^|~|<<|>>|in|not|is)\b/<span style="color:pink">\1<\/span>/g')
+    # Primero escapamos los caracteres HTML especiales
+    line=$(echo "$line" | sed -e 's/&/\&amp;/g' -e 's/</\&lt;/g' -e 's/>/\&gt;/g')
 
+    # Luego resaltamos operadores (incluyendo &lt; y &gt;)
+    line=$(echo "$line" | sed -E 's/(&lt;=|&gt;=|==|!=|=|\+=|-=|\*=|\/=|\*\*=|\/\/|%|\+|-|\*|\/|&amp;|\||\^|~|&lt;|&gt;|&lt;&lt;|&gt;&gt;)/<span style="color:red">\1<\/span>/g')
+    
+    highlighted=$(echo "$line" | sed -E 's/\b(and|or|not|in|is)\b/<span style="color:red">\1<\/span>/g')
 
-    echo "$highlighted" >> "$output"
+    echo "$line" >> "$output"
 done < "$input"
 
 echo "</pre></body></html>" >> "$output"
+
